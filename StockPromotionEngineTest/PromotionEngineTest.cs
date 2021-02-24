04106d84d278187c9b93b8add7a6813d5279347f
PragatiSaha <pragatisaha1993@gmail.com>
2021-02-24 17:14:57 +0530

    Added test method

--- StockPromotionEngineTest/PromotionEngineTest.cs
+++ StockPromotionEngineTest/PromotionEngineTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using StockPromotionEngine;
+using System.IO;
 
 namespace StockPromotionEngineTest
 {
@@ -11,34 +12,62 @@ namespace StockPromotionEngineTest
         [TestMethod]
         public void TestMethod1()
         {
-            CalculatePromotion calculatePromotion = new CalculatePromotion();
-            calculatePromotion.ActivePromotionCalculation(1, 1, 1, 0);
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                CalculatePromotion calculatePromotion = new CalculatePromotion();
+                calculatePromotion.ActivePromotionCalculation(1, 1, 1, 0);
+                var result = sw.ToString();
+                NUnit.Framework.Assert.AreEqual("100", result);
+            }
         }
         [TestMethod]
         public void TestMethod2()
         {
-            CalculatePromotion calculatePromotion = new CalculatePromotion();
-            calculatePromotion.ActivePromotionCalculation(5, 5, 1, 0);
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                CalculatePromotion calculatePromotion = new CalculatePromotion();
+                calculatePromotion.ActivePromotionCalculation(5, 5, 1, 0);
+                var result = sw.ToString();
+                NUnit.Framework.Assert.AreEqual("370", result);
+            }
         }
         [TestMethod]
         public void TestMethod3()
         {
-            CalculatePromotion calculatePromotion = new CalculatePromotion();
-            calculatePromotion.ActivePromotionCalculation(3, 5, 1, 1);
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                CalculatePromotion calculatePromotion = new CalculatePromotion();
+                calculatePromotion.ActivePromotionCalculation(3, 5, 1, 1);
+                var result = sw.ToString();
+                NUnit.Framework.Assert.AreEqual("280", result);
+            }
         }
         [TestMethod]
         public void TestMethod4()
         {
-            CalculatePromotion calculatePromotion = new CalculatePromotion();
-            calculatePromotion.ActivePromotionCalculation(3, 5, 7, 2);
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                CalculatePromotion calculatePromotion = new CalculatePromotion();
+                calculatePromotion.ActivePromotionCalculation(3, 5, 7, 2);
+                var result = sw.ToString();
+                NUnit.Framework.Assert.AreEqual("410", result);
+            }
         }
         [TestMethod]
         public void TestMethod5()
         {
-            CalculatePromotion calculatePromotion = new CalculatePromotion();
-            calculatePromotion.ActivePromotionCalculation(3, 5, 9, 7);
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                CalculatePromotion calculatePromotion = new CalculatePromotion();
+                calculatePromotion.ActivePromotionCalculation(3, 5, 9, 7);
+                var result = sw.ToString();
+                NUnit.Framework.Assert.AreEqual("500", result);
+            }
         }
-
-
     }
 }
